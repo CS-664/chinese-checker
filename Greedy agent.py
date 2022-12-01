55633f8e45a34a23e91ef73fcbfd2fe9e119c4c3
UITraTheno <62661973+UITraTheno@users.noreply.github.com>
2022-12-01 00:37:18 -0500

    update
    Greedy update

--- Greedy agent.py
+++ Greedy agent.py
@@ -1,15 +1,24 @@
-from player import *
-from board import *
-import enum
+import random
 
-class GameResult(enum.Enum):
-    rWin = 1
-    bWin = 2
-    gWin = 3
-    yWin = 4
-    oWin = 5
-    pWin = 6
+class GreedyAgent():
+    def move(self, pmoves):
+        best_move = []
+        length = 0
+        for move in pmoves:
+            if not best_move:
+                best_move.append(move)
+                length = abs(move.start_point.row - move.end_point.row)
+            else:
+                if abs(move.start_point.row - move.end_point.row) > length:
+                    best_move.clean()
+                    best_move.append(move)
+                    length = abs(move.start_point.row - move.end_point.row)
+                if abs(move.start_point.row - move.end_point.row) == length:
+                    best_move.append(move)
+                if abs(move.start_point.row - move.end_point.row) < length:
+                    continue
 
-def best_move(game):
-    if game.is_over():
-        if game.check_win(red):
+        return random.choice(best_move)
+
+
+

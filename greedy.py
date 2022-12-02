525cbabace9e53cf73ebd4870cdf495e24b1f399
UITraTheno <62661973+UITraTheno@users.noreply.github.com>
2022-12-02 16:13:01 -0500

    update
    Updated greedy

--- greedy.py
+++ greedy.py
@@ -1,14 +1,14 @@
 import random
 
 class GreedyAgent():
-    def move(self, pmoves):
+    def move(self, game):
         best_move = []
         depth = 0
-        for move in pmoves:
+        for move in game.potential_moves():
             start_depth = move.start_point.row + move.start_point.col
             end_depth = move.end_point.row + move.end_point.col
             if abs(end_depth - start_depth) > depth:
-                best_move.clean()
+                best_move.clear()
                 best_move.append(move)
                 depth = abs(end_depth-start_depth)
             if abs(end_depth-start_depth) == depth:
